35c9bb343d8e6fd849b3b7e909b4ecea06192713
Oles <olesbychevskyi@gmail.com>
2021-12-01 16:31:10 +0200

    init

---
+++ src/com/company/EncryptionProgram.java
@@ -0,0 +1,80 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class EncryptionProgram {
+
+    private Scanner scanner;
+    private Random random;
+    private ArrayList<Character> list;
+    private ArrayList<Character> shuffledList;
+    private char character;
+    private String line;
+    private char[] letters;
+    private char[] secretLetters;
+
+
+    EncryptionProgram() {
+        scanner = new Scanner(System.in);
+        random = new Random();
+        list = new ArrayList();
+        shuffledList = new ArrayList();
+        character = ' ';
+
+        newKey();
+        askQuestion();
+
+    }
+
+    private void askQuestion() {
+        while(true){
+            System.out.println("************************************************");
+            System.out.println("What do you want to do?");
+            System.out.println("(N)ewKey, (G)etKey, (E)ncrypt, (D)ecrypt, (Q)uit");
+            char response = Character.toUpperCase(scanner.nextLine().charAt(0));
+            switch (response) {
+                case 'N':
+                    newKey();
+                    break;
+                case 'G':
+                    getKey();
+                    break;
+                case 'E':
+                    encrypt();
+                    break;
+                case 'D':
+                    decrypt();
+                    break;
+                case 'Q':
+                    quit();
+                    break;
+                default:
+                    System.out.println("Not a valid answer");
+            }
+        }
+    }
+
+    private void newKey() {
+
+    }
+
+    private void getKey() {
+
+    }
+
+    private void encrypt() {
+
+    }
+
+    private void decrypt() {
+
+    }
+
+    private void quit() {
+
+    }
+
+
+}
